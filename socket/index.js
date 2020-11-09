180a35655925e9abf0ae2114da2b34a09e859968
Sanday08 <sandip.job89@gmail.com>
2020-11-09 15:57:08 +0530

    add

--- socket/index.js
+++ socket/index.js
@@ -59,10 +59,11 @@ io.on("connection", (socket) => {
 
 	});
 
-	socket.on("changeTurn",({currentSeatNo,roomId})=>{
+	socket.on("changeTurn",({currentSeatNo,roomId,isReTurn})=>{
 		console.log("dum dum");
 		let random=findRandom();
-		currentSeatNo=currentSeatNo===3?0:currentSeatNo+=1;
+		if(!isReTurn)
+			currentSeatNo=currentSeatNo===3?0:currentSeatNo+=1;
 		console.log("Current Cheat NO is:",currentSeatNo)
 		io.in(roomId).emit("res",{data:{seatNo:currentSeatNo,diceNo:findRandom()},en:"turn",status:1})
 		console.log("jaimin suthiyo....")
